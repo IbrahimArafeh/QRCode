bf74bac32ae7f6c4d9caab21742fd6a7ed3e03b8
Ibrahim Arafeh <68711291+IbrahimArafeh@users.noreply.github.com>
2022-07-23 20:30:45 +0400

    upload PDF File

--- QRCodeCR/QRCodeCR/Controllers/HomeController.cs
+++ QRCodeCR/QRCodeCR/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using QRCodeCR.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Drawing.Imaging;
 using System.IO;
@@ -8,6 +9,9 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using ZXing;
+using System.Configuration;
+using System.Data;
+using Dapper;
 
 namespace QRCodeCR.Controllers
 {
@@ -24,6 +28,7 @@ namespace QRCodeCR.Controllers
 
             return View();
         }
+
 
         public ActionResult Contact()
         {
@@ -52,7 +57,7 @@ namespace QRCodeCR.Controllers
             }
             return View("Index", qrcode);
         }
-
+        
         private string GenerateQRCode(string qrcodeText)
         {
             string folderPath = "~/Images/";
@@ -84,6 +89,7 @@ namespace QRCodeCR.Controllers
         public ActionResult Read()
         {
             return View(ReadQRCode());
+
         }
 
         private QRCodeModel ReadQRCode()
@@ -101,5 +107,100 @@ namespace QRCodeCR.Controllers
             }
             return new QRCodeModel() { QRCodeText = barcodeText, QRCodeImagePath = imagePath };
         }
+
+        #region Upload Download file  
+        public ActionResult FileUpload()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult FileUpload(HttpPostedFileBase files)
+        {
+            String FileExt = Path.GetExtension(files.FileName).ToUpper();
+
+            if (FileExt == ".PDF")
+            {
+                Stream str = files.InputStream;
+                BinaryReader Br = new BinaryReader(str);
+                Byte[] FileDet = Br.ReadBytes((Int32)str.Length);
+
+                FileDetailsModel Fd = new FileDetailsModel();
+                Fd.FileName = files.FileName;
+                Fd.FileContent = FileDet;
+                SaveFileDetails(Fd);
+                return RedirectToAction("FileUpload");
+            }
+            else
+            {
+
+                ViewBag.FileStatus = "Invalid file format.";
+                return View();
+
+            }
+
+        }
+
+        [HttpGet]
+        public FileResult DownLoadFile(int id)
+        {
+            List<FileDetailsModel> ObjFiles = GetFileList();
+
+            var FileById = (from FC in ObjFiles
+                            where FC.Id.Equals(id)
+                            select new { FC.FileName, FC.FileContent }).ToList().FirstOrDefault();
+
+            return File(FileById.FileContent, "application/pdf", FileById.FileName);
+
+        }
+        #endregion
+
+        #region View Uploaded files  
+        [HttpGet]
+        public PartialViewResult FileDetails()
+        {
+            List<FileDetailsModel> DetList = GetFileList();
+
+            return PartialView("FileDetails", DetList);
+        }
+        private List<FileDetailsModel> GetFileList()
+        {
+            List<FileDetailsModel> DetList = new List<FileDetailsModel>();
+
+            DbConnection();
+            con.Open();
+            DetList = SqlMapper.Query<FileDetailsModel>(con, "GetFileDetails", commandType: CommandType.StoredProcedure).ToList();
+            con.Close();
+            return DetList;
+        }
+
+        #endregion
+
+        #region Database related operations  
+        private void SaveFileDetails(FileDetailsModel objDet)
+        {
+
+            DynamicParameters Parm = new DynamicParameters();
+            Parm.Add("@FileName", objDet.FileName);
+            Parm.Add("@FileContent", objDet.FileContent);
+            DbConnection();
+            con.Open();
+            con.Execute("AddFileDetails", Parm, commandType: System.Data.CommandType.StoredProcedure);
+            con.Close();
+        }
+        #endregion
+
+        #region Database connection  
+
+        private SqlConnection con;
+        private string constr;
+        private void DbConnection()
+        {
+            constr = ConfigurationManager.ConnectionStrings["NIDB"].ToString();
+            con = new SqlConnection(constr);
+
+        }
+        #endregion
     }
+
 }
---
+++ QRCodeCR/QRCodeCR/Models/FileDetailsModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace QRCodeCR.Models
+{
+    public class FileDetailsModel
+    {
+        public int Id { get; set; }
+        [Display(Name = "Uploaded File")]
+        public String FileName { get; set; }
+        public byte[] FileContent { get; set; }
+    }
+}
